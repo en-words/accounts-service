3ce58d70da8fd21d0a95561c60997e35f4d3b757
noprysk <noprysk@n-ix.com>
2017-09-05 23:45:07 +0300

    First commit

--- src/main/java/en/words/accountsservice/AccountsServiceApplication.java
+++ src/main/java/en/words/accountsservice/AccountsServiceApplication.java
@@ -2,7 +2,9 @@ package en.words.accountsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@EnableGlobalMethodSecurity
 @SpringBootApplication
 public class AccountsServiceApplication {
 
--- src/test/java/en/words/accountsservice/AccountsServiceApplicationTests.java
+++ src/test/java/en/words/accountsservice/AccountsServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package en.words.accountsservice;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +13,11 @@ public class AccountsServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		RestTemplate restTemplate = new RestTemplate();
+		String greet = restTemplate.getForObject("http://localhost:3002/hello", String.class);
+		Assert.assertEquals("Hello World!!!", greet);
 	}
+
+
 
 }
